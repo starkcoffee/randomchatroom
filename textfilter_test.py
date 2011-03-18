d6a90969974672b21f301683e70eb17752f23b24
duana <strongcoffee2@gmail.com>
2011-03-18 10:51:42 +1000

    use a lastSubmitted timestamp to stop multiple accidental submits incase the page stops loading for unforseaable reason and then they cant submit anymore

--- textfilter_test.py
+++ textfilter_test.py
@@ -4,26 +4,17 @@ import textfilter
 class FormatLinksTest(unittest.TestCase):
 
     samples = ( 
-                ('check out http://foo.com', 
-                'check out <a href="http://foo.com">http://foo.com</a>'),
-
                 ('http://foo.com is great', 
                 '<a href="http://foo.com">http://foo.com</a> is great'),
 
                 ('hey http://foo.com is great', 
                 'hey <a href="http://foo.com">http://foo.com</a> is great'),
 
-                ('check out http://www.foo.com', 
-                'check out <a href="http://www.foo.com">http://www.foo.com</a>'),
-
                 ('http://www.foo.com is great', 
                 '<a href="http://www.foo.com">http://www.foo.com</a> is great'),
 
                 ('hey http://www.foo.com is great', 
                 'hey <a href="http://www.foo.com">http://www.foo.com</a> is great'),
-
-                ('check out www.foo.com', 
-                 'check out <a href="http://www.foo.com">www.foo.com</a>'),
 
                 ('www.foo.com is great', 
                  '<a href="http://www.foo.com">www.foo.com</a> is great'),
